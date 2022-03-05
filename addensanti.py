3420dd29ae4829792536cc012d12fb47e22910a6
Art <99206393+Guybrush-87@users.noreply.github.com>
2022-03-05 09:09:03 +0100

    Update addensanti.py

--- addensanti.py
+++ addensanti.py
@@ -68,6 +68,6 @@ pca_df=preprocessing.scale(pca_df)
 pca_ = pd.DataFrame(pca_df,index=nomi,columns=('viscosità','overrun'))
 
 st.table(pca_)
-st.plotly_chart(pca_.plot.scatter())
+st.plotly_chart(pca_.plot.scatter(x='viscosità',y='overrun'))
 
 
